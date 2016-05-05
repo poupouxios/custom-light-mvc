69a824ab702f90b041a7bf2f6d895f5520c5d188
Valentinos <vpapasavvas@gmail.com>
2016-05-05 23:17:18 +0100

    removed airbrake for the moment

--- public/config/config.php
+++ public/config/config.php
@@ -1,7 +1,6 @@
 <?php
 
 define('FILESYSTEM_PATH', realpath(dirname(__FILE__). DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR));
-//define('AIRBRAKE_API_KEY',"ed8b53e1514785e0e0b6c21fb0e05adc");
 
 $app_env = getenv('APPLICATION_ENV');
 if(!isset($app_env) || strlen($app_env) == 0){
@@ -22,20 +21,6 @@ if($app_env == 'local'){
 	define("BASE_URL","http://192.168.50.200:81/");
 }
 
-#if(ENABLE_ERRBIT){
-#	$airbrake_config = [
-#		"environmentName" => $app_env,
-#		"url" => "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]",
-#		"host" => "errbit.llamadigital.net",
-#		"secure" => true,
-#		"action" => "",
-#		"component" => "",
-#		'timeout' => 2,
-#		"errorReportingLevel" => ini_get('error_reporting')
-#	];
-#}
-
-
 function stb_autoloader($class_name) {
 	$classesDir = [
 	FILESYSTEM_PATH.'/application/',
@@ -47,8 +32,8 @@ function stb_autoloader($class_name) {
 
 	foreach ($classesDir as $directory) {   
 		if (file_exists($directory . $class_name . '.php')) {
-			require_once ($directory . $class_name . '.php');                 
-			return;         
+			require_once ($directory . $class_name . '.php');
+			return;
 		}
 	} 
 } 
