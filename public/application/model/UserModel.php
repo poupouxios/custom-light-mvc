bfe366d23fc881783bc902f53c643a43d5e98dec
Valentinos <vpapasavvas@gmail.com>
2016-05-18 18:45:06 +0100

    changed the expesnses to set a positive number as in mobile it didnt give the option to put negative numbers - changed the view calculation of users statitics to show the correct remaining balance with the new expense change

--- public/application/model/UserModel.php
+++ public/application/model/UserModel.php
@@ -1,32 +1,32 @@
 <?php
-	class UserModel extends BaseModel{
-	
-		public static $attributes = array(
-			"id" => 0,
-			"title" => "",
-			"firstname" => "",
-			"surname" => "",
-			"telephone" => "",
-			"email" => "",
-			"created_at" => "",
-			"updated_at" => "");
-		
-		public function calculateTotalExpenses(){
-			$expenses = ExpenseModel::getMapper()->findAllExpensesByUserId($this->id);
-			$total_expenses = 0;
-			foreach($expenses as $expense){
-				$total_expenses += floatval($expense->expense_value);
-			}
-			return $total_expenses;
-		}
+  class UserModel extends BaseModel{
+  
+    public static $attributes = array(
+      "id" => 0,
+      "title" => "",
+      "firstname" => "",
+      "surname" => "",
+      "telephone" => "",
+      "email" => "",
+      "created_at" => "",
+      "updated_at" => "");
+    
+    public function calculateTotalExpenses(){
+      $expenses = ExpenseModel::getMapper()->findAllExpensesByUserId($this->id);
+      $total_expenses = 0;
+      foreach($expenses as $expense){
+        $total_expenses += floatval($expense->expense_value);
+      }
+      return abs($total_expenses);
+    }
 
-		public function calculateTotalSavings(){
-			$savings = SavingModel::getMapper()->findAllSavingsByUserId($this->id);
-			$total_savings = 0;
-			foreach($savings as $saving){
-				$total_savings += floatval($saving->saving_value);
-			}
-			return $total_savings;
-		}
+    public function calculateTotalSavings(){
+      $savings = SavingModel::getMapper()->findAllSavingsByUserId($this->id);
+      $total_savings = 0;
+      foreach($savings as $saving){
+        $total_savings += floatval($saving->saving_value);
+      }
+      return $total_savings;
+    }
 
-	}
+  }
