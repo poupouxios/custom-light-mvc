3ce6add7ee9625968eb7826dddc84f65fc70dcba
Valentinos <vpapasavvas@gmail.com>
2016-05-18 18:26:47 +0100

    added port forwarding in vagrantfile for the mailcathcer - added some checks in the bootstrap if the array has any action parameters

--- Vagrantfile
+++ Vagrantfile
@@ -6,12 +6,15 @@ Vagrant.configure('2') do |config|
 
   config.vm.hostname = 'light-mvc'
   config.vm.network :private_network, ip: '192.168.69.95'
+  config.vm.network "forwarded_port", guest: 1080, host: 8080 
+  config.vm.network "forwarded_port", guest: 1025, host: 8025 
  
   config.vm.synced_folder '~', '/home/master'
   config.vm.synced_folder '.', '/home/vagrant/source'
- 
+  config.vm.boot_timeout = 1000;
+  
   config.vm.provider :virtualbox do |vb|
-    vb.customize ['modifyvm', :id, '--cpus', '2', '--memory', 1024]
+    vb.customize ['modifyvm', :id, '--cpus', '1', '--memory', 1024]
   end
  
   config.vm.provision :ansible do |ansible|
--- public/application/Bootstrap.php
+++ public/application/Bootstrap.php
@@ -6,55 +6,57 @@ class Bootstrap
 
   public function __construct($request){
     $this->request = $request;
-		$this->view = $this->callHook();
+    $this->view = $this->callHook();
   }
 
   public function render(){
     echo $this->view;
   }
 
-	function callHook() {
-			$urlArray = array();
-			$urlArray = explode("/",$this->request);
+  public function callHook() {
+    $urlArray = array();
+    $urlArray = explode("/",$this->request);
 
-			if(!isset($urlArray[0]) || strlen($urlArray[0]) == 0){
-				$view = new BaseView();
-				return $view->getContentView("home.html");
-			}
-			$controller = $urlArray[0];
-			array_shift($urlArray);
+    if(!isset($urlArray[0]) || strlen($urlArray[0]) == 0){
+      $view = new BaseView();
+      return $view->getContentView("home.html");
+    }
+    $controller = $urlArray[0];
+    array_shift($urlArray);
 
-			$action = $urlArray[0];
-			array_shift($urlArray);
-			
-      $queryString = array();
-			while(isset($urlArray[0])){
-	  		$queryString[] = $urlArray[0];
-				array_shift($urlArray);
-	    }
-		
-			$controllerName = $controller;
-			$controller = ucwords($controller);
-			$model = rtrim($controller, 's')."Model";
-			$controller .= 'Controller';
-			$this->controller = $controller;
+    $action = "";
+    $queryString = array();
+    if(isset($urlArray[0])){
+      $action = $urlArray[0];
+      array_shift($urlArray);
+      
+      while(isset($urlArray[0])){
+        $queryString[] = $urlArray[0];
+        array_shift($urlArray);
+      }
+    }
+      
+    $controllerName = $controller;
+    $controller = ucwords($controller);
+    $model = rtrim($controller, 's')."Model";
+    $controller .= 'Controller';
+    $this->controller = $controller;
 
-			if (class_exists($model))
-			{
-        //echo $model."<br/>".$controller."<br/>".$action."<br/>".print_r($queryString);
-				$dispatch = new $controller();
-			}
+    if (class_exists($model))
+    {
+      $dispatch = new $controller();
+    }
 
-			if ((int)method_exists($controller, $action)) {
-				return call_user_func_array(array($dispatch,$action),$queryString);
-			} else {
-				return "";
-			}
-		}
+    if ((int)method_exists($controller, $action)) {
+      return call_user_func_array(array($dispatch,$action),$queryString);
+    } else {
+      return "";
+    }
+  }
 
-		private function getWrapperTemplate(){
-		  $wrapper = file_get_contents('application/template.html');
-		  return $wrapper;
-		}
+  private function getWrapperTemplate(){
+    $wrapper = file_get_contents('application/template.html');
+    return $wrapper;
+  }
 
 }
